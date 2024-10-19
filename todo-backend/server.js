6eee68f354b7951a4aee90f63645142384461458
Your Name <you@example.com>
2024-10-19 12:44:01 +0530

    new commit

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -5,11 +5,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-const PORT = 8000;
+const PORT = 5000;
 
 // connnecting mongodb
 mongoose
-  .connect(process.env.MONGO_URI || "mongodb+srv://karthimad7707:333@items.hfewo.mongodb.net/to-do-app?retryWrites=true&w=majority&appName=Items")
+  .connect(process.env.MONGO_URI || "mongodb+srv://karthimad7707:333@items.hfewo.mongodb.net/mern-app?retryWrites=true&w=majority&appName=Items")
   .then(() => {
     console.log("Database connected");
   })
