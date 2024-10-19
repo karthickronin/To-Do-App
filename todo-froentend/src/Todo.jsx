b9a84a3f0ded9155101e2b04577246a9891bf0c7
Your Name <you@example.com>
2024-10-19 13:00:36 +0530

    .

--- todo-froentend/src/Todo.jsx
+++ todo-froentend/src/Todo.jsx
@@ -11,7 +11,7 @@ export default function Todo() {
   const [editTitle, setEditTitle] = useState("karthick");
   const [editDescription, setEditDescription] = useState("ronin");
 
-  const API = "http://localhost:8000";
+  const API = "https://to-do-app-5-kc2d.onrender.com";
 
   const handleSubmit = () => {
     if (title.trim() !== "" && description.trim() !== "") {
